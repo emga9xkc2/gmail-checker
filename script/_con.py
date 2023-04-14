4f4218fc7cd1e37a03f1709bc9af3735d648eb8f
emga9xkc2 <android.24081995@gmail.com>
2023-04-14 20:23:51 +0700

    commit

---
+++ script/_con.py
@@ -0,0 +1,5 @@
+
+from hini import *
+ini = hini()
+title = "Gmail Checker"
+url_tool = "https://github.com/emga9xkc2/gmail-checker/archive/refs/heads/main.zip"
---
+++ web/function.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    loadDashboard();
+    $.getScript("/codinglab.js", function () {});
+});
+
+function loadDashboard() {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: "html/dashboard.html",
+            success: function (result) {
+                $("#load-dashboard").html(result);
+                resolve();
+            },
+            error: function (error) {
+                reject(error);
+            },
+        });
+    });
+}
+
+function openMenu(evt, cityName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("tabs");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(
+            " menuselected",
+            ""
+        );
+    }
+    document.getElementById(cityName).style.display = "block";
+    // console.log(evt.currentTarget)
+    evt.currentTarget.className += " menuselected";
+    var linkName = evt.currentTarget.querySelector(".link_name").textContent;
+    document.getElementById("title").textContent = linkName;
+}
+
+function msgBox(text) {
+    alert(text);
+}
+
+eel.getTitle()(function (callback) {
+    // console.log(callback);
+    document.title = callback;
+});
